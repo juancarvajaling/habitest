36ead9108ec6a6ad6327133b7c8ad7dbfb90304e
Juan Carvajal <juancarvajaling@gmail.com>
2022-04-12 00:53:13 -0500

    Fix uncomplited SQL query

--- habiapi/propertyfinder/tests/test_data/query_no_filters.sql
+++ habiapi/propertyfinder/tests/test_data/query_no_filters.sql
@@ -3,8 +3,13 @@ SELECT
     status.name as status, property.price as price,
     property.description as description
 FROM property
+INNER JOIN (
+    SELECT property_id, status_id, MAX(update_date) max_date
+    FROM status_history
+    GROUP BY property_id
+) recent_status ON property.id=recent_status.property_id
 INNER JOIN status_history
     ON property.id=status_history.property_id
 INNER JOIN status
-    ON status_history.status_id=status.id
+    ON recent_status.status_id=status.id
 WHERE status.name IN ("pre_venta", "en_venta", "vendido")
